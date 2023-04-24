72661c6fe576771af4f8db2062a013a73c2bf822
jimJunhyun <tsumego2015@gmail.com>
2023-04-24 20:29:44 +0900

    bridge render 90%
    height examination needed

---
+++ WhyNotPJ_RTS/Assets/Scripts/General/Groundbreaking/BridgeRender.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeRender : MonoBehaviour
+{
+    List<Renderer> bridgeUnder = new List<Renderer>(); //다리 -> 시작점 -> 끝점
+	//BoxCollider valueChanger;
+	Material mat;
+
+	public const float UNITHEIGHT = 1.5f;
+
+	private void Awake()
+	{
+		GetComponentsInChildren<Renderer>(bridgeUnder);
+		//List<BoxCollider> bs = new List<BoxCollider>();
+		//bridgeUnder[0].GetComponentsInChildren<BoxCollider>(bs);
+		//bs.Remove(bridgeUnder[0].GetComponent<BoxCollider>());
+		//valueChanger = bs[0];
+
+
+		//valueChanger.transform.position -= new Vector3(0, UNITHEIGHT, 0);
+
+		mat = new Material(bridgeUnder[0].material);
+
+		for (int i = 0; i < bridgeUnder.Count; i++)
+		{
+			bridgeUnder[i].material = mat;
+		}
+	}
+
+	public void Gen(float leng)
+	{
+		mat.mainTextureScale = new Vector2(1, leng / 10);
+		
+		bridgeUnder[0].transform.localScale = new Vector3(100, 35 * leng / 3, 100);
+		bridgeUnder[1].transform.localPosition = Vector3.forward * (leng / 2);
+		bridgeUnder[2].transform.localPosition = -Vector3.forward * (leng / 2);
+	}
+}
---
+++ WhyNotPJ_RTS/Assets/Scripts/Managers/ConstructBuild.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Buildables
+{
+	Bridge,
+	Wall,
+
+
+}
+
+public class ConstructBuild : MonoBehaviour
+{ 
+
+	const float BRIDGEXSCALE = 4.5f;
+	const float BRIDGEYSCALE = 0.5f;
+	const int GROUNDLAYERMASK = 1 << 8;
+
+	const float RAYGAP = 1.2f; //모델 지름
+
+
+	public float bridgeYErr = 0.5f;
+	public BridgeRender bridge;
+	//public GameObject wall;
+
+	//test
+	public Transform sPos;
+	public Transform ePos;
+	//test
+
+	private void Start()
+	{
+		Construct(sPos.position, ePos.position, Buildables.Bridge);
+	}
+
+	public void Construct(Vector3 startPos, Vector3 endPos, Buildables type)
+	{
+		// 클릭시스템과 병합 시 제거
+		Vector2Int sIdx = Perceive.PosToIdxVector(startPos);
+		Vector2Int eIdx = Perceive.PosToIdxVector(endPos);
+		startPos.y = PlayerEye.instance.perceived.map[sIdx.y, sIdx.x].height;
+		endPos.y = PlayerEye.instance.perceived.map[eIdx.y, eIdx.x].height;
+		// 클릭시스템과 병합 시 제거
+		Vector3 dir = endPos - startPos;
+		Vector3 pos = (startPos + endPos) / 2;
+		float dist = dir.magnitude;
+		if(type == Buildables.Bridge)
+		{
+			if(BridgeExamine(startPos, endPos, dist))
+			{
+				BridgeRender b = Instantiate(bridge);
+				b.transform.position = pos;
+				b.transform.LookAt(endPos);
+				b.Gen(dist);
+			}
+			else
+			{
+				Debug.Log("지을 수 없다.");
+			}
+		}
+		
+	}
+	//사용 안될듯?
+	//public void ConstructWithIdx(Vector2Int startPos, Vector2Int endPos, Buildables type)
+	//{
+	//	Vector3 sPos = Perceive.IdxVectorToPos(startPos);
+	//	Vector3 ePos = Perceive.IdxVectorToPos(endPos);
+	//	sPos.y = PlayerEye.instance.perceived.map[startPos.y, startPos.x].height;
+	//	ePos.y = PlayerEye.instance.perceived.map[endPos.y, endPos.x].height;
+	//	Vector3 dir = ePos - sPos;
+	//	Vector3 pos = (sPos + ePos) / 2;
+	//	float dist = dir.magnitude;
+	//	if (type == Buildables.Bridge)
+	//	{
+	//		BridgeRender b = Instantiate(bridge);
+	//		b.transform.position = pos;
+	//		b.transform.LookAt(ePos);
+	//		b.Gen(dist);
+	//	}
+	//}
+
+	bool BridgeExamine(Vector3 startPos, Vector3 endPos, float length)
+	{
+		Vector3 dir = endPos - startPos;
+		startPos.y -= BRIDGEYSCALE / 2 - bridgeYErr;
+		if(Physics.BoxCast(startPos, new Vector3(BRIDGEXSCALE / 2, BRIDGEYSCALE / 2, 0.5f), dir.normalized, Quaternion.LookRotation(dir.normalized), length, GROUNDLAYERMASK))
+		{
+			return false;
+		}
+		for (int i = 0; i < length; i++)
+		{
+
+		}
+		return true;
+	}
+}
