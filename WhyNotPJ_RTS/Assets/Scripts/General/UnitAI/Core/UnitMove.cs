8705fce74f1fbb826b09f5d3e1987714320e19c0
Taegon Lee <ltg9amer@naver.com>
2023-05-10 00:16:09 +0900

    Non-Routine 2023/05/10
    유닛 리깅 애니메이션 적용

--- WhyNotPJ_RTS/Assets/Scripts/General/UnitAI/Core/UnitMove.cs
+++ WhyNotPJ_RTS/Assets/Scripts/General/UnitAI/Core/UnitMove.cs
@@ -5,14 +5,18 @@ using UnityEngine.AI;
 public class UnitMove : MonoBehaviour
 {
     private NavMeshHit hit;
+    private Transform visualTrm;
     private NavMeshAgent navMeshAgent;
     public NavMeshAgent NavMeshAgent => navMeshAgent;
     private NavMeshPath path;
+    private Animator animator;
 
     private void Start()
     {
+        visualTrm = transform.GetChild(1);
         navMeshAgent = GetComponent<NavMeshAgent>();
         path = new NavMeshPath();
+        animator = visualTrm.GetComponent<Animator>();
     }
 
     public void SetTargetPosition(Vector3 pos)
@@ -30,13 +34,24 @@ public class UnitMove : MonoBehaviour
         switch (hit.mask)
         {
             case 8:
+
                 navMeshAgent.speed = moveSpeed * 0.5f/*0.5f는 기획에 따라 변경 가능*/;
 
                 break;
             default:
                 navMeshAgent.speed = moveSpeed;
+                visualTrm.localPosition = Vector3.zero;
 
                 break;
+        }
+
+        if (navMeshAgent.velocity.sqrMagnitude >= 0.1f)
+        {
+            animator.SetBool("isWalk", true);
+        }
+        else
+        {
+            animator.SetBool("isWalk", false);
         }
     }
 }
